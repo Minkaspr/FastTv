11dd1cb3acd9ff74ec3db36c9992e14438718325
Dario <106852390+Minkaspr@users.noreply.github.com>
2024-04-06 16:21:17 -0500

    initial project - be

---
+++ fasttv-be/src/main/java/com/mk/fasttvbe/FasttvBeApplication.java
@@ -0,0 +1,13 @@
+package com.mk.fasttvbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasttvBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasttvBeApplication.class, args);
+	}
+
+}
---
+++ fasttv-be/src/test/java/com/mk/fasttvbe/FasttvBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.fasttvbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasttvBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
